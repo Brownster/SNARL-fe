4fb0c6703a5492a355875007383f0f53f1162f5a
Marc Brown <marcsmobile.brown@googlemail.com>
2023-04-16 11:24:28 +0100

    Update snarl_fe.py

--- snarl_fe.py
+++ snarl_fe.py
@@ -2,9 +2,160 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 from datetime import datetime
 import os
+import base64
+from io import BytesIO
 
-# Import the functions from your existing code
-from alert_functions import count_alerts_per_node, count_alerts_per_node_day, plot_alerts_per_node_day, count_top_additional_information, Heat_count_alerts_per_node_hour
+
+def Heat_count_alerts_per_node_hour(file_path, start_date=None, end_date=None):
+    # Read in the alert data from a CSV file
+    df = pd.read_csv(file_path)
+
+    # Convert the 'Initial event generation time' column to datetime
+    df['Initial event generation time'] = pd.to_datetime(df['Initial event generation time'])
+
+    # Filter the data by date range, if provided
+    if start_date and end_date:
+        mask = (df['Initial event generation time'] >= start_date) & (df['Initial event generation time'] <= end_date)
+        df = df.loc[mask]
+
+    # Extract the hour of the day from the 'Initial event generation time' column
+    df['Hour of Day'] = df['Initial event generation time'].dt.hour
+
+    # Group the data by node and hour of the day, then count the number of alerts
+    count_per_node_hour = df.groupby(['Node', 'Hour of Day'])['Number'].count().unstack()
+
+    # Fill NaN values with 0
+    count_per_node_hour = count_per_node_hour.fillna(0).astype(int)
+
+    # Plot a heatmap of the results
+    fig, ax = plt.subplots(figsize=(10, 8))
+    sns.heatmap(count_per_node_hour, annot=True, fmt='d', ax=ax)
+    ax.set_xlabel("Hour of Day")
+    ax.set_ylabel("Node")
+    ax.set_title("Number of Alerts per Node per Hour of Day")
+
+    # Save the figure as a PNG image
+    buffer = BytesIO()
+    fig.savefig(buffer, format='png')
+    buffer.seek(0)
+    heatmap_img = base64.b64encode(buffer.getvalue()).decode()
+
+    plt.close(fig)
+
+    return heatmap_img
+
+
+
+
+
+def count_top_additional_information(file_path, start_date=None, end_date=None):
+    # Read in the alert data from a CSV file
+    df = pd.read_csv(file_path)
+
+    # Convert the 'Initial event generation time' column to datetime
+    df['Initial event generation time'] = pd.to_datetime(df['Initial event generation time'])
+
+    # Filter the data by date range, if provided
+    if start_date and end_date:
+        mask = (df['Initial event generation time'] >= start_date) & (df['Initial event generation time'] <= end_date)
+        df = df.loc[mask]
+
+    # Group the data by 'Additional information' and count the number of alerts
+    count_per_additional_info = df.groupby('Additional information')['Number'].count()
+
+    # Sort the counts in descending order and get the top 10
+    top_10_additional_info = count_per_additional_info.sort_values(ascending=False).head(10)
+
+    # Display the results in a table using PrettyTable
+    table = PrettyTable()
+    table.field_names = ["Additional Information", "Number of Alerts"]
+    for info, count in top_10_additional_info.items():
+        table.add_row([info, count])
+    print(table)
+
+
+
+def plot_alerts_per_node_day(file_path, start_date=None, end_date=None):
+    # Read in the alert data from a CSV file
+    df = pd.read_csv(file_path)
+
+    # Convert the 'Initial event generation time' column to datetime
+    df['Initial event generation time'] = pd.to_datetime(df['Initial event generation time'])
+
+    # Filter the data by date range, if provided
+    if start_date and end_date:
+        mask = (df['Initial event generation time'] >= start_date) & (df['Initial event generation time'] <= end_date)
+        df = df.loc[mask]
+
+    # Extract the day of the week from the 'Initial event generation time' column
+    df['Day of Week'] = df['Initial event generation time'].dt.day_name()
+
+    # Group the data by node and day of the week, then count the number of alerts
+    count_per_node_day = df.groupby(['Node', 'Day of Week'])['Number'].count().unstack()
+
+    # Fill NaN values with 0
+    count_per_node_day = count_per_node_day.fillna(0).astype(int)
+
+    # Create a bar plot using matplotlib
+    ax = count_per_node_day.plot(kind='bar', stacked=True, figsize=(10, 5))
+    ax.set_ylabel('Number of Alerts')
+    ax.set_title('Alerts per Node by Day of Week')
+
+    # Display the plot
+    plt.show()
+
+
+
+def count_alerts_per_node_day(file_path, start_date=None, end_date=None):
+    # Read in the alert data from a CSV file
+    df = pd.read_csv(file_path)
+
+    # Convert the 'Initial event generation time' column to datetime
+    df['Initial event generation time'] = pd.to_datetime(df['Initial event generation time'])
+
+    # Filter the data by date range, if provided
+    if start_date and end_date:
+        mask = (df['Initial event generation time'] >= start_date) & (df['Initial event generation time'] <= end_date)
+        df = df.loc[mask]
+
+    # Extract the day of the week from the 'Initial event generation time' column
+    df['Day of Week'] = df['Initial event generation time'].dt.day_name()
+
+    # Group the data by node and day of the week, then count the number of alerts
+    count_per_node_day = df.groupby(['Node', 'Day of Week'])['Number'].count().unstack()
+
+    # Fill NaN values with 0
+    count_per_node_day = count_per_node_day.fillna(0).astype(int)
+
+    # Display the results in a table using PrettyTable
+    table = PrettyTable()
+    table.field_names = ["Node"] + list(count_per_node_day.columns)
+    for node, row in count_per_node_day.iterrows():
+        table.add_row([node] + row.tolist())
+    print(table)
+
+###### counts per cust
+def count_alerts_per_node(file_path, start_date=None, end_date=None):
+    # Read in the alert data from a CSV file
+    df = pd.read_csv(file_path)
+
+    # Filter the data by date range, if provided
+    if start_date and end_date:
+        mask = (df['Initial event generation time'] >= start_date) & (df['Initial event generation time'] <= end_date)
+        df = df.loc[mask]
+
+    # Group the data by node and count the number of alerts for each node
+    count_per_node = df.groupby('Node')['Number'].count()
+
+    # Display the results in a table using PrettyTable
+    table = PrettyTable()
+    table.field_names = ["Node", "Number of Alerts"]
+    for node, count in count_per_node.items():
+        table.add_row([node, count])
+    print(table)
+
+
+
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
@@ -75,6 +226,18 @@ def index():
             flash(str(e))
 
     return render_template('index.html')
+
+@app.route('/heatmap', methods=['POST'])
+def heatmap():
+    file_path = request.form['file_path']
+    start_date = request.form['start_date']
+    end_date = request.form['end_date']
+
+    heatmap_img = Heat_count_alerts_per_node_hour(file_path, start_date, end_date)
+
+    return render_template('heatmap.html', heatmap_img=heatmap_img)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8010)
